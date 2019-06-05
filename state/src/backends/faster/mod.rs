aa5b69a335cc20eba11c3ea164dc58df77665c50
Matthew Brookes <mbrookes1304@gmail.com>
2019-06-05 14:30:18 +0200

    Print store size on checkpoint

--- state/src/backends/faster/mod.rs
+++ state/src/backends/faster/mod.rs
@@ -22,6 +22,7 @@ pub struct FASTERBackend {
 fn maybe_refresh_faster(faster: &Rc<FasterKv>, monotonic_serial_number: u64) {
     if monotonic_serial_number % (1 << 12) == 0 {
         faster.checkpoint().unwrap();
+        println!("FASTER Store Size: {}", faster.size());
     }
     if monotonic_serial_number % (1 << 8) == 0 {
         faster.complete_pending(false);
