8e96db2c0f8c1c205ae2edeaddba507908d3d9b4
Matthew Brookes <mbrookes1304@gmail.com>
2019-07-17 14:21:48 +0200

    Create FASTER InMemory Backend

--- state/src/backends/mod.rs
+++ state/src/backends/mod.rs
@@ -1,7 +1,9 @@
 pub use faster::FASTERBackend;
+pub use faster_in_memory::FASTERInMemoryBackend;
 pub use in_memory::InMemoryBackend;
 pub use in_memory_native::InMemoryNativeBackend;
 
 mod faster;
+mod faster_in_memory;
 mod in_memory;
 mod in_memory_native;
