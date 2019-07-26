832dc02f6552ba763a372ef0c79502f094f1b9dd
Matthew Brookes <mbrookes1304@gmail.com>
2019-07-26 10:12:22 +0200

    Try checkpoint less often

--- state/src/backends/faster/mod.rs
+++ state/src/backends/faster/mod.rs
@@ -27,7 +27,7 @@ pub struct FASTERBackend {
 }
 
 fn maybe_refresh_faster(faster: &Arc<FasterKv>, monotonic_serial_number: u64) {
-    if monotonic_serial_number % (1 << 20) == 0 {
+    if monotonic_serial_number % (1 << 22) == 0 {
         faster.checkpoint();
     } else if monotonic_serial_number % (1 << 6) == 0 {
         faster.complete_pending(true);
