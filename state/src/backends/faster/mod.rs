ac6418b84e01311286b72640ea4949fdb837533d
Matthew Brookes <mbrookes1304@gmail.com>
2019-08-01 10:31:27 +0200

    faster: checkpoint every 60

--- state/src/backends/faster/mod.rs
+++ state/src/backends/faster/mod.rs
@@ -98,7 +98,7 @@ impl StateBackend for FASTERBackend {
         let faster_kv_clone = Arc::clone(&faster_kv);
         std::thread::spawn(move || {
             loop {
-                std::thread::sleep(Duration::from_secs(30));
+                std::thread::sleep(Duration::from_secs(60));
                 let checkpoint = faster_kv_clone.checkpoint();
                 match checkpoint {
                     Ok(c) => println!("Checkpoint token: {}", c.token),
