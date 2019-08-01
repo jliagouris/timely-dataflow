f524c934645abb1eb55cf1a1575aa45d75147f2d
Matthew Brookes <mbrookes1304@gmail.com>
2019-08-01 14:41:27 +0200

    Revert "faster_node: checkpoint every 120s"
    This reverts commit 1e1bb9be1d6d98762c05b5d24240e9fdd3938eea.

--- timely/src/execute.rs
+++ timely/src/execute.rs
@@ -196,7 +196,7 @@ where
     let faster_kv_clone = Arc::clone(&faster_kv);
     std::thread::spawn(move || {
         loop {
-            std::thread::sleep(Duration::from_secs(120));
+            std::thread::sleep(Duration::from_secs(60));
             let checkpoint = faster_kv_clone.checkpoint();
             match checkpoint {
                 Ok(c) => println!("Checkpoint token: {}", c.token),
